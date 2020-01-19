e18aa6463e4177ad230dd21327d3d21131a6f170
jgalli <joelgalli@gmx.ch>
2020-01-20 00:46:56 +0100

    Implement division test function

--- src/test/java/ch/bbw/jg/ConsoleCalculatorWithMaven/CalculatorTest.java
+++ src/test/java/ch/bbw/jg/ConsoleCalculatorWithMaven/CalculatorTest.java
@@ -16,12 +16,12 @@ import org.junit.Test;
 public class CalculatorTest {
 
 	private Calculator testCalculator;
-	
+
 	@Before
 	public void setup() {
 		testCalculator = new Calculator();
 	}
-	
+
 	int int1 = 10;
 	int int2 = 20;
 
@@ -29,20 +29,20 @@ public class CalculatorTest {
 	public void testSumOfTwoPositiveIsOk() {
 		assertTrue(testCalculator.sum(int1, int2) == 30);
 	}
-	
+
 	@Test
 	public void testSubstrOfTwoPositiveIsOk() {
 		assertTrue(testCalculator.subtr(int1, int2) == -10);
 	}
-	
+
 	int int1Neg = -10;
 	int int2Neg = -20;
-	
+
 	@Test
 	public void testSumOfTwoNegativeIsOk() {
 		assertTrue(testCalculator.sum(int1Neg, int2Neg) == -30);
 	}
-	
+
 	@Test
 	public void testSubstrOfTwoNegativeIsOk() {
 		assertTrue(testCalculator.subtr(int1Neg, int2Neg) == 10);
